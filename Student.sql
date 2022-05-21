6efc08eb96d9a068022baef3de567f96d971377e
hoàng0809 <91303390+luongviethoang@users.noreply.github.com>
2022-05-21 11:06:07 +0700

    done

---
+++ Student.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE Students;
+
+Use Students;
+
+Create table Student(
+                        StudenID int,
+                        Name varchar(50),
+                        Address varchar(50),
+                        phone int
+);
+
+INSERT into Student VALUES(101,'luong viet khanh','Ha Long',0978902242);
+INSERT into Student VALUES(102,'Nguyen Ba Hoang','Ha Noi',0978902299);
+INSERT into Student VALUES(103,'Doan Duy Kien','Thai Binh',0955302242);
+INSERT into Student VALUES(104,'Dinh Quang Linh','Nam Dinh',0978902442);
+INSERT into Student VALUES(105,'Nguyen Manh Quy','Tay Nguyen',0978902242);
+INSERT into Student VALUES(106,'Luong Quang Truong','Hai phong',0978902242);
+INSERT into Student VALUES(107,'Khanh Ba Kha','Tay Bac',0978934242);
+INSERT into Student VALUES(108,'Sung a Hoang','Ho Chi MInh',0978903242);
+INSERT into Student VALUES(109,'Trang A Lua','Da Nang',0978902242);
+
+
